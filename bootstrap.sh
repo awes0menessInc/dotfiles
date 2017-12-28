5c33b9e4490b8cc689cf66d6e0811fb8ec081451
awes0menessInc <shiv_bajpayi@yahoo.co.in>
2017-12-28 15:57:17 -0500

    Added bootstrap.sh

---
+++ bootstrap.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+# Asking for sudo permission
+[ "$UID" -eq 0 ] || exec sudo bash "$0" "$@"
+
+# In case the directory is not the shell file directory
+cd `dirname $0` 
+
+mkdir $HOME/old_dotfiles # Backup directory to prevent blunders
+mv $HOME/.* $HOME/old_dotfiles
+
+cp -ai . $HOME # Copy dotfiles
+
+# Black magic on part of homebrew. Bundle automatically identifies the Brewfile
+# Then proceeds to install all the packages.
+
+brew bundle 
+brew cleanup
+brew prune
+
+
+# Copies preferences if OS is MacOS
+if [[ "$OSTYPE" == "darwin"* ]]; then
+		mkdir $HOME/old_preferences # Backup for Blunders
+		cp -r /Users/$USER/Library/Preferences $HOME/old_preferences
+		mv -r user_preferences/* /Users/$USER/Library/Preferences # Copy all my MacOS and app preferences
+fi
+
+# Get rid of evidence
+rm -ir user_preferences
+rm Brewfile
+
+clear
+
+echo "ALL DONE!"
+
+/bin/rm -f "${0}"
